1d608a0ffd228c119cdacffb3b314a3e885ab4ad
Bakhram <abdurakhimov@list.ru>
2022-06-09 17:28:41 +0500

    errorLog infoLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("address", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -16,8 +19,12 @@ func main() {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	log.Printf("Запуск сервера на %v", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	svr := &http.Server{
+		Addr:     *addr,
+		Handler:  mux,
+		ErrorLog: errorLog,
+	}
+	infoLog.Printf("Запуск сервера на %v", *addr)
+	err := svr.ListenAndServe()
+	errorLog.Fatal(err)
 }
