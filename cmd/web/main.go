84bd9c6ccc38dc243321231b4711701187da1d5d
Bakhram <abdurakhimov@list.ru>
2022-06-11 22:34:44 +0500

    added driver mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -13,27 +15,37 @@ type application struct {
 }
 
 func main() {
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=1", "Сетевой адрес веб-сервера")
 	addr := flag.String("address", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDb(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	svr := &http.Server{
 		Addr:     *addr,
-		Handler:  mux,
 		ErrorLog: errorLog,
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Запуск сервера на %v", *addr)
-	err := svr.ListenAndServe()
+	err = svr.ListenAndServe()
 	errorLog.Fatal(err)
+}
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
